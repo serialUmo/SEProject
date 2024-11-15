b64cd1c85cceee4026b3f68e541400e6a7d6a8c6
serialUmo <116769462+serialUmo@users.noreply.github.com>
2024-11-14 22:07:53 -0500

    Update adminoptions.php

--- site/adminoptions.php
+++ site/adminoptions.php
@@ -1,1 +1,16 @@
+<?php
+  session_start(); // Start the session
+  
+  // Check if the user is logged in
+  if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+      header("Location: adminlogin.html"); // Redirect to login if not logged in
+      exit();
+  }
+?>
 
+<html>
+<body>
+<a href="adminview.php">Manage Requests and Appointments</a>
+<a href="admingallery.php">Manage Gallery</a>
+</body>
+</html>
