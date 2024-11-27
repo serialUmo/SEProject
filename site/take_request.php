dcf2b6135d635fbc1908224c5668dbfca298bb11
serialUmo <116769462+serialUmo@users.noreply.github.com>
2024-11-26 22:24:57 -0500

    Add delete function

--- site/take_request.php
+++ site/take_request.php
@@ -15,15 +15,22 @@
 <h2>Enter Appointment Details</h2>
 <form action="save_appointment.php" method="POST">
   <input type="hidden" name="RequestID" value="<?php echo $requestID; ?>">
-  
+
   <label for="date">Appointment Date (YYYY-MM-DD):</label>
   <input type="text" name="date">
-  
+
   <label for="cost">Cost: $</label>
   <input type="text" name="cost"><br>
-  
+
   <label for="description">Description:</label>
   <input type="text" name="desc"><br>
-  
+
   <button type="submit">Save Appointment</button>
+</form>
+
+<h2>Delete Request</h2>
+<!-- Delete Request Form -->
+<form action="delete_request.php" method="POST" onsubmit="return confirm('Are you sure you want to delete this request?');">
+  <input type="hidden" name="RequestID" value="<?php echo $requestID; ?>">
+  <button type="submit" style="color: red;">Delete Request</button>
 </form>
